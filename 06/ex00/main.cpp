e5cae96585f2dbffb7e80621f34419d34366713b
pderksen98 <pieter.derksen@hotmail.com>
2023-06-30 13:01:17 +0200

    beginning module 06

--- 06/ex00/main.cpp
+++ 06/ex00/main.cpp
@@ -1,55 +1,63 @@
 #include <iostream>
+#include <string>
+#include "Colors.hpp"
 
-// class Base {
-//     public:
-//         Base() {}
-//         virtual ~Base() {}
-// };
-
-// class Derived : public Base {
-//     public:
-//         Derived() {}
-//         ~Derived() {}
-// };
-
-// class AnotherClass : public Base {
-//     public:
-//         AnotherClass() {}
-//         ~AnotherClass() {}
-// };
-
-// int main2()
-// {
-//     double value = 5.25;
-    
-//     double a = (int)value + 5.3;                //C style cast  
-//     double s = static_cast<int>(value) + 5.3;  //static cast
-
-//     // double s = reinterpret_cast<AnotherClass*>(&value); //reinterpret cast
-     
-
-//     Derived* derived = new Derived();
-//     Base* base = derived;
-
-//     AnotherClass* ac = dynamic_cast<AnotherClass*>(base );
-
-//     std::cout << a << std::endl;
-//     std::cout << s << std::endl;
-//     return 0;
-// }
-
-int* test()
+int main(int argc, char* argv[]) 
 {
-    int x = new(sizeof(int));
-    *x = 4;
-    std::cout << "addres of int is: " << &x << std::endl;
-    return (x);
-}
+    if (argc != 2) {
+        std::cout << "Wrong number of arguments: expected 1, got " << argc - 1 << std::endl;
+        return (1);
+    }
+    std::string input(argv[1]);
+    std::string type;
 
-int main() {
-    int *check = test();
-    std::cout << "addres of int is: " << check << std::endl;
-    std::cout << *check << std::endl;
 
-   return (0);
+    if (input.length() == 1 && !std::isdigit(input.front())) {
+        type = "char";
+    }
+    else if (input.find('.') != std::string::npos) {
+        if (input.back() == 'f')
+            type = "float";
+        else
+            type = "double";
+    }
+    else {
+        type = "int";
+    }
+    std::cout << GREEN << "TYPE: " << type << PLAIN << std::endl;
+    if (type == "char") {
+        char c = input[0];
+        if (!std::isprint(c))
+            std::cout << "char: Non displayable" << std::endl;
+        else {
+            std::cout << "char: " << c << std::endl;
+            std::cout << "int: " << static_cast<int>(c) << std::endl;
+            std::cout << "float: " << static_cast<float>(c) << ".0f" << std::endl;
+            std::cout << "double: " << static_cast<double>(c) << ".0" << std::endl;
+        }
+    } else if (type == "int") {
+        int i = std::stoi(input);
+        std::cout << "char: " << static_cast<char>(i) << std::endl;
+        std::cout << "int: " << i << std::endl;
+        std::cout << "float: " << static_cast<float>(i) << ".0f" << std::endl;
+        std::cout << "double: " << static_cast<double>(i) << ".0" << std::endl;
+    } else if (type == "float") {
+        float f = std::stof(input);
+        std::cout << "char: " << static_cast<char>(f) << std::endl;
+        std::cout << "int: " << static_cast<int>(f) << std::endl;
+        std::cout << "float: " << f << "f" << std::endl;
+        std::cout << "double: " << static_cast<double>(f) << std::endl;
+    } else if (type == "double") {
+        std::cout << "input = " << input << std::endl;
+        double d = std::stod(input);
+        std::cout << "double d = " << d << std::endl;
+        std::cout << "char: " << static_cast<char>(d) << std::endl;
+        std::cout << "int: " << static_cast<int>(d) << std::endl;
+        std::cout << "float: " << static_cast<float>(d) << "f" << std::endl;
+        std::cout << "double: " << d << std::endl;
+    }
+
+
+
+    return (0);
 }
