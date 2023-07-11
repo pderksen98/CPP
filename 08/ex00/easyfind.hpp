fcd76d7a5076834121f105897c9560c0ab6c76ef
Pieter Derksen <pieterderksen@MacBook-Pro-van-Pieter.local>
2023-07-11 12:14:29 +0200

    progress with module 08

--- 08/ex00/easyfind.hpp
+++ 08/ex00/easyfind.hpp
@@ -1,8 +1,26 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
 template <typename T>
-typename T::iterator easyfind(T &container, int value)
+typename T::iterator    easyfind(T& container, int value) 
 {
-    typename T::iterator it = std::find(container.begin(), container.end(), value);
-    if (it == container.end())
-        throw std::exception();
-    return it;
+    typename T::iterator it = container.begin();
+    while (it != container.end()) {
+        if (*it == value)
+            return it;
+        it++;
+    }
+    throw std::runtime_error("Could not find int value in container");
 }
+
+// template <typename T>
+// typename T::iterator easyfind(T &container, int value)
+// {
+//     typename T::iterator it = std::find(container.begin(), container.end(), value);
+//     if (it == container.end())
+//         throw std::runtime_error("Could not find int value in container");
+//     return it;
+// }
+
+
+#endif
--- 08/ex00/main.cpp
+++ 08/ex00/main.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
 #include <vector>
-#include <unordered_map>
-#include <algorithm>
+// #include <algorithm>
+#include "easyfind.hpp"
 
 int main()
 {
-    std::vector<int> values = { 1, 2, 3, 4, 5 };
+    //C++11 feature to use initializer list
+    // std::vector<int> numbers = {1, 2, 3, 4, 5};
 
-    for (int i = 0; i < (int)values.size(); i++) {
-        std::cout << values[i] << std::endl;
-    }
-    // for (int value : values) {
-    //     std::cout << value << std::endl;
-    // }
-    for (std::vector<int>::iterator it = values.begin(); it != values.end(); it++) {
-        std::cout << *it << std::endl;
-    }
-    
-    using ScoreMap = std::unordered_map<std::string, int>;
-    ScoreMap map;
-    map["Hello"] = 5;
-    map["World"] = 2;
-    
-    for (ScoreMap::const_iterator it = map.begin(); it != map.end(); it++) {
-        std::cout << it->first << " : " << it->second << std::endl;
+    std::vector<int> numbers;
+    numbers.push_back(1);
+    numbers.push_back(2);
+    numbers.push_back(3);
+    numbers.push_back(4);
+    numbers.push_back(5);
+
+    try {
+        std::vector<int>::iterator result = easyfind(numbers, 3);
+        std::cout << "Value found at position " << std::distance(numbers.begin(), result) << std::endl;
+    } catch (std::runtime_error &e) {
+        std::cerr << e.what() << std::endl;
     }
 
+    try {
+        std::vector<int>::iterator result = easyfind(numbers, 6);
+        std::cout << "Value found at position " << std::distance(numbers.begin(), result) << std::endl;
+    } catch (std::runtime_error &e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return (0);
 }
